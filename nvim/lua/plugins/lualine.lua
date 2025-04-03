f01fc3eba8c96c1063709878e7665f4bced71f96
mark <abrakadabra@example.com>
2025-04-03 23:46:05 +0500

    fix: remove the deprecated function "clients_lsp" in lualine.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,28 +2,11 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = function()
-        local clients_lsp = function()
-            local bufnr = vim.api.nvim_get_current_buf()
-            local clients = vim.lsp.buf_get_clients(bufnr)
-            if next(clients) == nil then
-                return ""
-            end
-            local c = {}
-            for _, client in pairs(clients) do
-                table.insert(c, client.name)
-            end
-            return "\u{f085} " .. table.concat(c, "|")
-        end
         return {
-            refresh = {
-                statusline = 100,
-                tabline = 1000,
-                winbar = 1000,
-            },
             sections = {
                 lualine_b = { "branch", "diff" },
                 lualine_c = {
-                    clients_lsp,
+                    "lsp_status",
                     {
                         "diagnostics",
                         sources = { "nvim_diagnostic" },
